4bfbd23d2c148d52ec4d75ae262658408b5cf5ad
harrysink <113678645+harrysink@users.noreply.github.com>
2023-09-22 13:12:50 +0100

    Delete src/components/Card.js

--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import React, {forwardRef} from 'react';
-
-export const Card = forwardRef({src, title, id, index, ...props}, ref) => {
-    return (
-        <div className='card'>
-            <img src={src} alt={title} />
-        </div>
-    )
-}
